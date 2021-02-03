7439ac900635d8e3d595ea82c5b42cc4ec87d8ad
James Woolfenden <James.Woolfenden@gmail.com>
2021-02-03 14:00:09 +0000

    for act

--- example/examplea/module.appsync.tf
+++ example/examplea/module.appsync.tf
@@ -4,9 +4,9 @@ module "appsync" {
   lambda_resolver_arn  = var.lambda_resolver_arn
   datasource_user_name = var.datasource_user_name
   api_name             = var.api_name
-  request_template     = file("templates/request.json")
-  response_template    = file("templates/response")
-  schema               = file("templates/schema")
+  request_template     = file("${path.module}/templates/request.json")
+  response_template    = file("${path.module}/templates/response")
+  schema               = file("${path.module}/templates/schema")
   resolver_field       = "singlePost"
   resolver_type        = "Query"
 }
