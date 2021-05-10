0ec631ad998afbc3d8dec690c9d62a35ce7f8da9
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-10 11:44:30 +0100

    not json

--- example/examplea/module.appsync.tf
+++ example/examplea/module.appsync.tf
@@ -4,7 +4,7 @@ module "appsync" {
   lambda_resolver_arn  = var.lambda_resolver_arn
   datasource_user_name = var.datasource_user_name
   api_name             = var.api_name
-  request_template     = file("${path.module}/templates/request.json")
+  request_template     = file("${path.module}/templates/request")
   response_template    = file("${path.module}/templates/response")
   schema               = file("${path.module}/templates/schema")
   resolver_field       = "singlePost"
